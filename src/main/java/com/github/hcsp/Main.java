6db6d9865cd21644fcead29a7f3caf58324329f7
hufan <hufan30@163.com>
2020-03-09 23:36:21 +0800

    Create Main.java

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,12 @@
+package com.github.hcsp;
+
+/**
+ * @description:
+ * @author: HuFan
+ * @time: 2020/3/911:12 下午
+ **/
+public class Main {
+    public static void main(String[] args) {
+        new MiniJVM(null,null);
+    }
+}
